7a9debb01fcc7259cff91ace4da56133f0ab699a
Pasindu Perera <pasindubawantha@gmail.com>
2018-07-01 20:32:48 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "";  // server to connect to.
+$db_name = "group_db";  // the name of the database.
+$db_user = "root";  // mysql username to access the database with.
+$db_pass = "";  // mysql password to access the database with.
+$db_table = "";    // the table that this script will set up and use.
+?>
